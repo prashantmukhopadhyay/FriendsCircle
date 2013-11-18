a2639eaf3bd9fc16e317b4feff0018b59a45b367
Dale+Prashant <Dale+Prashant@appacademy.io>
2013-11-18 10:32:59 -0500

    finishes sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
 
+  def new
+    render :new
+  end
+
+  def create
+    user = User.new(params[:user])
+    if user.save
+      login_user!
+    else
+      flash[:error] = "try again"
+
+      redirect_to new_user_url
+    end
+  end
+
   def show
     render text: "right place"
   end
